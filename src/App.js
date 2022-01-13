d6034d43d2efc24b591a1979555b7072b4467adb
Lucia Z <sweet@lucias-mbp.lan>
2022-01-13 09:01:27 -0600

    Create modules

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import {useCallback} from 'react';
-import {useDropzone} from 'react-dropzone';
-
-function MyDropzone() {
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles);
-  }, []);
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
-  )
-}
+import CsvDropzone from './components/CsvDropzone';
 
 function App() {
   return (
     <div className="App">
-      <MyDropzone />
+      <CsvDropzone />
     </div>
   );
 }
