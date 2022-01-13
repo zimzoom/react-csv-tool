0e88573ff4ab271859d224604805371b7f4cb0e6
Lucia Z <sweet@lucias-mbp.lan>
2022-01-13 10:37:57 -0600

    Fix indent

--- src/components/CsvDropzone.js
+++ src/components/CsvDropzone.js
@@ -6,46 +6,46 @@ import Papa from 'papaparse';
 function CsvDropzone() {
 	const [parsedCsvData, setParsedCsvData] = useState([]);
 
-  const parseFile = file => {
-  	Papa.parse(file, {
-  		header: true,
-  		complete: results => {
-  			console.log("parsed!")
-  			console.log(results.data)
-  			setParsedCsvData(results.data)
-  		},
-  	});
-  };
+	const parseFile = file => {
+		Papa.parse(file, {
+			header: true,
+			complete: results => {
+				console.log("parsed!")
+				console.log(results.data)
+				setParsedCsvData(results.data)
+			},
+		});
+	};
 
-  const onDrop = useCallback(acceptedFiles => {
-    if (acceptedFiles.length) {
-    	console.log(acceptedFiles[0]);
-    	parseFile(acceptedFiles[0]);
-    }
-  }, []);
+	const onDrop = useCallback(acceptedFiles => {
+	if (acceptedFiles.length) {
+		console.log(acceptedFiles[0]);
+		parseFile(acceptedFiles[0]);
+	}
+	}, []);
 
-  const {
-  	getRootProps, 
-  	getInputProps, 
-  	isDragActive, 
-  	isDragAccept, 
-  	isDragReject
-  } = useDropzone({onDrop, accept: 'text/csv'})
+	const {
+		getRootProps, 
+		getInputProps, 
+		isDragActive, 
+		isDragAccept, 
+		isDragReject
+	} = useDropzone({onDrop, accept: 'text/csv'})
 
-  return (
-    <div {...getRootProps({
-    	className: `dropzone 
-    	${isDragAccept && 'fileAccept'} 
-    	${isDragReject && 'fileReject'}`
-    })}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop here ...</p> :
-          <p>Drag 'n' drop a CSV file here, or click to select file</p>
-      }
-    </div>
-  )
+	return (
+		<div {...getRootProps({
+			className: `dropzone 
+			${isDragAccept && 'fileAccept'} 
+			${isDragReject && 'fileReject'}`
+		})}>
+		  <input {...getInputProps()} />
+		  {
+		    isDragActive ?
+		      <p>Drop here ...</p> :
+		      <p>Drag 'n' drop a CSV file here, or click to select file</p>
+		  }
+		</div>
+	)
 }
 
 export default CsvDropzone;
